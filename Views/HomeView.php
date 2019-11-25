720be7e6741522e47f2480556c248999c7e95462
johnnyl25 <lemesle.johnny@live.fr>
2019-11-25 15:40:18 +0100

    homeview ajout

--- Views/HomeView.php
+++ Views/HomeView.php
@@ -1,23 +1,40 @@
 <?php
 
 $title = "Home";
-// include 'header.php';
+include 'header.php';
 
 ?>
 
 <div id="main" class="">
-        <h2>Liste des films</h2>
-        <ul>
-            <?php 
-            foreach ($listeFilms as $film) :
-                echo '<li>'.$film['titre'].'</li>';
-            endforeach;
-            ?>
-        </ul>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <h2> Listes des Films</h2>
+        </div>
+        <div class="row justify-content-center m-2">
+        <?php
+        foreach($listeFilms as $film) :
+        $idFilm = $film['id_films'];
+        ?>
+        <div class="col-md-4">
+            <a href="index.php?page=Film&idFilm=<?php echo $idFilm ?>" target="_blank">
+                <div class="card">
+                    <img src="assets/images/<?php echo $film['affiche'] ?>" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $film['titre'] ?></h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <?php
+        endforeach;
+        ?>
+        </div>
+    </div>
 </div>
 
 <?php
 
 // include 'contact-form.php';
 
-// include 'footer.php';
+include 'footer.php';
