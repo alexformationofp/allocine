5cb2e7a8a7b0e99b91a579fcf5bf584698fb5d13
alexformationofp <55798552+alexformationofp@users.noreply.github.com>
2019-11-29 13:29:47 +0100

    debut css

--- Views/ConnexionUserView.php
+++ Views/ConnexionUserView.php
@@ -3,8 +3,9 @@ $title = "Ciné-117 : Connexion / inscription";
 include 'header.php';
 ?>
 <div class="container">
-    <div class="row">
+    <div class="row mt-5">
         <div class="connexion col-sm-5 fondgris">
+            <h3>Connectez-vous :</h3>
             <?php if (isset($_GET['erreur'])) { ?>
                 <div class="erreur">
                     <p>Erreur dans l'identification</p>
@@ -12,26 +13,45 @@ include 'header.php';
 
             <?php } ?>
             <form action="?page=ConnexionUser&provenance=<?php echo $provenance; ?>" method="post">
-                <label for="emailInput">Votre email :</label>
-                <input type="text" name="emailInput" id="emailInput">
-                <label for="passwordInput">Mot de passe :</label>
-                <input type="password" name="passwordInput" id="passwordInput">
-                <input type="submit" value="Connexion">
+                <div class="champ">
+                    <label for="emailInput">Votre email :</label>
+                    <input type="text" name="emailInput" id="emailInput">
+                </div>
+                <div class="champ">
+                    <label for="passwordInput">Mot de passe :</label>
+                    <input type="password" name="passwordInput" id="passwordInput">
+                </div>
+                <div class="champ">
+                     <input type="submit" value="Connexion">
+                </div>              
             </form>
         </div>
-
-        <div class="inscription col-sm-6 offset-sm-1 fondgris">
+        <div class="col-sm-2 ou">
+            <p>ou</p>
+        </div>
+        <div class="inscription col-sm-5 fondgris">
+            <h3>Inscrivez-vous :</h3>
             <form action="?page=ConnexionUser" method="post">
-
-                <label for="nouveauPrenom">Votre prénom :</label>
-                <input type="text" name="nouveauPrenom" id="nouveauPrenom">
-                <label for="nouveauNom">Votre nom :</label>
-                <input type="text" name="nouveauNom" id="nouveauNom">
-                <label for="nouveauEmail">Votre e-mail :</label>
-                <input type="text" name="nouveauEmail" id="nouveauEmail">
-                <label for="nouveauPassword">Votre mot de passe</label>
-                <input type="password" name="nouveauPassword" id="nouveauPassword">
-                <input type="submit" value="S'inscrire">
+                <div class="champ">
+                    <label for="nouveauPrenom">Votre prénom :</label>
+                    <input type="text" name="nouveauPrenom" id="nouveauPrenom">
+                </div>
+                <div class="champ">
+                    <label for="nouveauNom">Votre nom :</label>
+                    <input type="text" name="nouveauNom" id="nouveauNom">
+                </div>
+                <div class="champ">
+                    <label for="nouveauEmail">Votre e-mail :</label>
+                    <input type="text" name="nouveauEmail" id="nouveauEmail">
+                </div>
+                <div class="champ">
+                    <label for="nouveauPassword">Votre mot de passe</label>
+                    <input type="password" name="nouveauPassword" id="nouveauPassword">
+                </div>
+                <div class="champ">
+                    <input type="submit" value="S'inscrire">
+                </div>
+                
 
             </form>
         </div>
--- Views/HomeView.php
+++ Views/HomeView.php
@@ -7,21 +7,21 @@ include 'header.php';
 
 <div id="main" class="">
 
-    <div class="container fondgris pt-5 pb-2">
+    <div class="container fondgris homeview pt-2 pt-sm-5 pb-2">
         <div class="row justify-content-center">
-            <h2 class="titreFilms mb-4"> Listes des Films</h2>
+            <h2 class="titreFilms mb-4"> Liste des Films</h2>
         </div>
         <div class="row justify-content-center mb-3 mt-3">
             <?php
             foreach ($listeFilms as $film) :
                 $idFilm = $film['id_films'];
                 ?>
-                <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="col-6 col-md-4 col-lg-3">
                     <a href="index.php?page=Film&idFilm=<?php echo $idFilm ?>">
-                        <div class="card m-2">
-                            <img src="assets/images/<?php echo $film['affiche'] ?>" class="card-img-top" alt="">
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $film['titre'] ?></h5>
+                        <div class="carteFilm">
+                            <img src="assets/images/<?php echo $film['affiche'] ?>" alt="">
+                            <div class="titreFilm">
+                                <h5><?php echo $film['titre'] ?></h5>
                             </div>
                         </div>
                     </a>
